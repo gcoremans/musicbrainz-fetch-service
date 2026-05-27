010b9811f6821fcc3b3d62604bee7ebb33d42a1f
Gilles Coremans <gilles.coremans@redpencil.io>
2026-05-27 16:15:06 +0200

    Fetch service functional
    After a batch has finished fetching, mu:uuid will be assigned and the
    triples added to the triplestore

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // see https://github.com/mu-semtech/mu-javascript-template for more info
 
-import { app, query, errorHandler } from 'mu';
+import { app, update, errorHandler, beforeExit } from 'mu';
+import bodyParser from 'body-parser';
 import { Namespace, graph, parse, serialize } from 'rdflib';
 
 const SCHEMA = Namespace("http://schema.org/");
@@ -20,9 +21,10 @@ function muFetch(url, opts) {
         opts = {};
     }
     if (!opts.headers) {
-        opts.headers = [];
+        opts.headers = new Headers();
     }
-    opts.headers["User-Agent"] = "mu-musicbrainz-fetch-service/0 (gilles.coremans@redpencil.io)";
+    opts.headers.set("User-Agent", "mu-musicbrainz-fetch-service/0 (gilles.coremans@redpencil.io)");
+    opts.headers.set("Accept", "application/ld+json");
     return fetch(url, opts); // not async, we just return the promise directly
 }
 
@@ -56,13 +58,21 @@ function enqueue(item) {
         batches[batch].jobs += 1;
         batches[batch].fetched.push(item.uuid);
     }
+    console.log(`Pushing ${item.kind}/${item.uuid} to queue`);
     fetchQueue.push(item)
     if (!running) {
+        console.log("Starting processing");
+        running = true;
         setTimeout(process, currentDelay);
     }
 }
 
 async function process() {
+    if (fetchQueue.length == 0) {
+        console.log("Process called on empty queue!")
+        running = false;
+        return;
+    }
     let item = fetchQueue.shift();
     const uuid = item.uuid;
     const kind = item.kind;
@@ -71,42 +81,39 @@ async function process() {
     console.log(`Fetching ${kind}/${uuid}, remaining queue length ${fetchQueue.length}`);
 
     let response = await muFetch(`https://musicbrainz.org/${kind}/${uuid}`);
+    let body = await response.text();
 
     if (response.status != 200) {
-        console.error(`MB request failed for ${kind}/${uuid} with ${response.status} ${response.error}`);
+        console.log(`MB request failed for ${kind}/${uuid} with ${response.status} ${response.error}`);
+        console.log(body);
     } else {
         const store = batches[batch].store;
-        await parseP(jlds, store, defaultGraph, "application/ld+json")
+        await parseP(body, store, defaultGraph, "application/ld+json")
         if(kind == "artist") {
             // If this is the URI we're looking up
             if (uuid == batch) {
                 // Fetch all its albums (release-groups)
                 store.match(itemToUri(item), SCHEMA('album'), null, null)
-                    .map(s => s.object.value)
-                    .map(uriToItem)
+                    .map(s => uriToItem(s.object.value, batch))
                     .forEach(enqueue);
             }
             // Otherwise, skip
         } else if (kind == "release-group") {
             // Fetch all releases of this album
             store.match(itemToUri(item), SCHEMA('albumRelease'), null, null)
-                .map(s => s.object.value)
-                .map(uriToItem)
+                .map(s => uriToItem(s.object.value, batch))
                 .forEach(enqueue);
             // Fetch all the artists of this album
             store.match(itemToUri(item), SCHEMA('byArtist'), null, null)
-                .map(s => s.object.value)
-                .map(uriToItem)
+                .map(s => uriToItem(s.object.value, batch))
                 .forEach(enqueue);
         } else if (kind == "release") {
             // If this is the URI we're looking up
             if (uuid == batch) {
                 // Fetch this release's release group (this should only return one triple, but we use match() and maps anyway)
                 store.match(itemToUri(item), SCHEMA('releaseOf'), null, null)
-                    .map(s => s.object.value)
-                    .map(uriToItem)
+                    .map(s => uriToItem(s.object.value, batch))
                     .forEach(enqueue);
-
             }
             // Else, skip
         }
@@ -122,26 +129,55 @@ async function process() {
     // If this was the last job in the batch, finish it and import it into the triplestore
     if (batches[batch].jobs <= 0) {
         if (batches[batch].jobs < 0) {
-            console.error(`Batch ${batch} has impossible ${batches[batch].jobs} jobs!`);
+            console.error(`Batch ${batch} has impossible ${batches[batch].jobs} job count!`);
         } else {
-            console.log(batches[batch].store.toNT());
+            console.log(`Finishing batch ${batch} with ${batches[batch].fetched.length} fetches.`)
+            const store = batches[batch].store;
+
+            // Copy item UUIDs to mu:uuid
+            const resources = [].concat(
+                store.match(null, RDF('type'), SCHEMA('MusicGroup'), null),
+                store.match(null, RDF('type'), SCHEMA('MusicAlbum'), null),
+                store.match(null, RDF('type'), SCHEMA('MusicRelease'), null),
+                store.match(null, RDF('type'), SCHEMA('MusicRecording'), null))
+                .map(t => uriToItem(t.subject.value, null));
+                
+            resources.forEach(item => store.add(itemToUri(item), MU('uuid'), item.uuid, null));
+
+            // Insert items into triplestore
+            //const data = await serializeP(null, store, defaultGraph, "text/turtle", { flags: "p" });
+            const data = store.match(null, null, null, null)
+                .filter(t => t.subject.termType != "BlankNode" && t.object.termType != "BlankNode")
+                .map(t => t.toNT());
+            const sparqlData =
+`
+INSERT DATA {
+    GRAPH <${defaultGraph}> {
+        ${data.join("\n")}
+    }
+}
+`
+            await update(sparqlData);
+            console.log(`Batch ${batch} finished. Added ${data.length} triples to the store.`);
         }
     }
     
     if (fetchQueue.length != 0) {
         setTimeout(process, currentDelay);
     } else {
+        console.log("Processing finished.");
         running = false;
     }
 }
 
-function uriToItem(uri) {
-    const regex = /http:\/\/musicbrainz.org\/(?<kind>artist|recording|release|release-group)\/(?<uuid>[\w-]+)/;
+function uriToItem(uri, batch) {
+    const regex = /https?:\/\/musicbrainz.org\/(?<kind>artist|recording|release|release-group)\/(?<uuid>[\w-]+)/;
     const matches = uri.match(regex);
     if (matches) {
         return {
             kind: matches.groups.kind,
-            uuid: matches.groups.uuid
+            uuid: matches.groups.uuid,
+            batch: (batch ? (batch) : (matches.groups.uuid))
         };
     } else {
         return null;
@@ -152,23 +188,18 @@ function itemToUri(item) {
     return MB(`${item.kind}/${item.uuid}`);
 }
 
-app.get('/', async function( req, res ) {
-    let response = await muFetch("https://musicbrainz.org/artist/e58c654e-cde5-4fa2-a356-9204475a748e", { headers: new Headers({ "Accept": "application/ld+json" }) });
-    let jld = await response.json();
-    let jlds = JSON.stringify(jld)
-    console.log(jld);
+app.post('/fetch', bodyParser.json({ limit: '1mb' }), async function( req, res ) {
+    const item = uriToItem(req.body.uri);
+    enqueue(item);
+    res.json(item);
+});
 
-    let store = graph();
-    await parseP(jlds, store, defaultGraph, "application/ld+json");
-    console.log(store.toNT())
-    let albums = store.match(null, RDF('type'), SCHEMA('MusicAlbum'), null);
-    console.log(albums)
-    let subjects = albums.map(s => s.subject.value);
-    console.log(subjects)
-    let items = subjects.map(uriToItem)
-    console.log(items)
-    res.send(items);
-    
+app.get('/queue', async function( req, res ) {
+    res.json(fetchQueue);
+});
+
+app.get('/batches', async function( req, res ) {
+    res.json(batches);
 });
 
 app.use(errorHandler);
